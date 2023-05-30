3c6b57a1596e224f8c274d1aaa27c041c7147746
Arne de Bruijn <mail@arnedebruijn.nl>
2023-05-30 19:06:40 +0200

    Version 2023.05.29

--- LevelPost/Properties/AssemblyInfo.cs
+++ LevelPost/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2021.0623.0")]
+[assembly: AssemblyVersion("1.2023.0529.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
